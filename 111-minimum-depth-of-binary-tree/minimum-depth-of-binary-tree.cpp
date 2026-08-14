9cb6e35d0c4e44582b5ab7f4c9ce9de705d0bc1c
Rishabh Mishra <rishimis0706@gmail.com>
2026-08-14 23:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 146.8 MB (52.41%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 1});
+
+        while (!q.empty()) {
+            auto [node, depth] = q.front();
+            q.pop();
+
+            if (node->left == nullptr && node->right == nullptr) {
+                return depth;
+            }
+
+            if (node->left) q.push({node->left, depth + 1});
+            if (node->right) q.push({node->right, depth + 1});
+        }
+
+        return 0; // unreachable if root is not null
+    }
+};
