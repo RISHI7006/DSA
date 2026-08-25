768cb427bfb83d896a14a8a5479aeb29db46ffb2
Rishabh Mishra <rishimis0706@gmail.com>
2026-08-25 22:11:17 +0530

    Time: 12 ms (56.38%) | Memory: 21.9 MB (79.10%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,13 +5,8 @@ class Solution:
         groups = defaultdict(list)
 
         for s in strs:
-            # Count frequency of each character (26 lowercase letters)
-            count = [0] * 26
-            for char in s:
-                count[ord(char) - ord('a')] += 1
-
-            # Use tuple of counts as the key (lists aren't hashable)
-            key = tuple(count)
+            # Sort the characters to create a canonical key
+            key = ''.join(sorted(s))
             groups[key].append(s)
 
         return list(groups.values())
