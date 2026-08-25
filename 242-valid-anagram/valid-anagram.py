5f1e55d40883ee3eecc0e6f64d96469a2d8bc819
Rishabh Mishra <rishimis0706@gmail.com>
2026-08-25 22:14:58 +0530

    Time: 25 ms (31.03%) | Memory: 12.6 MB (67.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,9 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        count = [0] * 26
-
-        for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-
-        return all(c == 0 for c in count)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return Counter(s)==Counter(t)
         
