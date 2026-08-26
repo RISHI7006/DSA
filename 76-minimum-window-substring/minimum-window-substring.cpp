0ba3b9e69edee4e305f0ac7c251c613a1849d212
Rishabh Mishra <rishimis0706@gmail.com>
2026-08-26 21:24:51 +0530

    Time: 1 ms (83.71%) | Memory: 11.8 MB (16.18%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty()) return "";
+        if (s.empty() || t.empty() || s.size() < t.size()) return "";
 
-        unordered_map<char, int> need;
+        int need[128] = {0};
         for (char c : t) {
             need[c]++;
         }
 
-        int required = need.size();
-        int left = 0, formed = 0;
-        unordered_map<char, int> windowCounts;
+        int required = 0;
+        for (int i = 0; i < 128; i++) {
+            if (need[i] > 0) required++;
+        }
 
+        int window[128] = {0};
+        int formed = 0;
+        int left = 0, right = 0;
         int minLen = INT_MAX, minLeft = 0;
 
-        int right = 0;
-        while (right < s.size()) {
-            char c = s[right];
-            windowCounts[c]++;
+        int sLen = s.size();
 
-            if (need.count(c) && windowCounts[c] == need[c]) {
+        while (right < sLen) {
+            char c = s[right];
+            window[c]++;
+
+            if (need[c] > 0 && window[c] == need[c]) {
                 formed++;
             }
 
-            while (left <= right && formed == required) {
-                char leftChar = s[left];
-
+            while (formed == required) {
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     minLeft = left;
                 }
 
-                windowCounts[leftChar]--;
-                if (need.count(leftChar) && windowCounts[leftChar] < need[leftChar]) {
+                char leftChar = s[left];
+                window[leftChar]--;
+                if (need[leftChar] > 0 && window[leftChar] < need[leftChar]) {
                     formed--;
                 }
-
                 left++;
             }
 
