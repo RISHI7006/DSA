b5d2c991301c0855a09d57c48844f30b6fa6da00
Rishabh Mishra <rishimis0706@gmail.com>
2026-09-05 23:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.16%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long result = 1;
+        int N = m + n - 2;
+        int k = min(m - 1, n - 1);
+        for (int i = 1; i <= k; i++) {
+            result = result * (N - k + i) / i;
+        }
+        return (int)result;
+    }
+};
