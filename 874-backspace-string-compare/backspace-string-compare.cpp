457a8040e26ce2aa787e3b133206ddef73ecf204
Rishabh Mishra <rishimis0706@gmail.com>
2026-09-15 22:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.32%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,18 +1,48 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        auto build = [](const string& str) {
-            string stack;
-            for (char ch : str) {
-                if (ch != '#') {
-                    stack.push_back(ch);
-                } else if (!stack.empty()) {
-                    stack.pop_back();
+        int i = s.size() - 1, j = t.size() - 1;
+        int skipS = 0, skipT = 0;
+        
+        while (i >= 0 || j >= 0) {
+            // Find the next valid (non-backspaced) character in s
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
                 }
             }
-            return stack;
-        };
+            
+            // Find the next valid (non-backspaced) character in t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            
+            // Compare current valid characters
+            if (i >= 0 && j >= 0) {
+                if (s[i] != t[j]) return false;
+            } else if (i >= 0 || j >= 0) {
+                // One string still has a valid character but the other doesn't
+                return false;
+            }
+            
+            i--;
+            j--;
+        }
         
-        return build(s) == build(t);
+        return true;
     }
 };
