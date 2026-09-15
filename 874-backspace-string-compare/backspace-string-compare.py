857078cac9f40fc008870182e0a6c505bbe5f6c2
Rishabh Mishra <rishimis0706@gmail.com>
2026-09-15 22:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.82%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,40 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        i, j = len(s) - 1, len(t) - 1
+        skip_s, skip_t = 0, 0
+        
+        while i >= 0 or j >= 0:
+            # Find the next valid (non-backspaced) character in s
+            while i >= 0:
+                if s[i] == '#':
+                    skip_s += 1
+                    i -= 1
+                elif skip_s > 0:
+                    skip_s -= 1
+                    i -= 1
+                else:
+                    break
+            
+            # Find the next valid (non-backspaced) character in t
+            while j >= 0:
+                if t[j] == '#':
+                    skip_t += 1
+                    j -= 1
+                elif skip_t > 0:
+                    skip_t -= 1
+                    j -= 1
+                else:
+                    break
+            
+            # Compare current valid characters
+            if i >= 0 and j >= 0:
+                if s[i] != t[j]:
+                    return False
+            elif i >= 0 or j >= 0:
+                # One string still has a valid character but the other doesn't
+                return False
+            
+            i -= 1
+            j -= 1
+        
+        return True
